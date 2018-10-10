9c1b3eb67ddf42f73eb5a0c57442a5bbd56890d2
Will Huang <s951553@gmail.com>
2018-10-10 22:25:17 +0800

    Add problem 242 v2.

--- Problem_242_Valid_Anagram/main.cpp
+++ Problem_242_Valid_Anagram/main.cpp
@@ -2,14 +2,27 @@
 #include <string>
 
 bool isAnagram(std::string s, std::string t) {
-    int num = 0;
-    for (auto c : s) {
-        auto index = t.find_first_of(c);
-        if (index == std::string::npos)
+//    Version 1
+//    int num = 0;
+//    for (auto c : s) {
+//        auto index = t.find_first_of(c);
+//        if (index == std::string::npos)
+//            return false;
+//        t.erase(t.begin() + index);
+//    }
+//    return t.empty();
+    
+    // Version 2
+    int cnt[26] = {0};
+    for (auto c : s)
+        cnt[c - 'a']++;
+    for (auto c : t)
+        cnt[c - 'a']--;
+    for (int i = 0; i < 26; i++) {
+        if (cnt[i] != 0)
             return false;
-        t.erase(t.begin() + index);
     }
-    return t.empty();
+    return true;
 }
 
 int main(int argc, char **argv)
